e1e60ebc288ae173c34b7bb2608f721b51c4ca93
lvaladez1 <valadez140@gmail.com>
2026-03-29 17:43:28 -0700

    Update timezone

--- index.mjs
+++ index.mjs
@@ -5,34 +5,41 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     let apiKey = "HyHWUGZuTcPcy1-WqusGh9FC_ZoZmzyn9ZcLNwsuFGU";
     let url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&query=solar-system`;
-    
+
     let response = await fetch(url);
     let data = await response.json();
     let randomImage = data.urls.full;
-    res.render('index', {"image":randomImage});
+    res.render('index', { "image": randomImage });
 });
 
 app.get('/planet', (req, res) => {
     let planetName = req.query.planetName;
     let planetInfo = planets[`get${planetName}`]();
     console.log(planetInfo);
-    res.render('planet', {planetInfo, planetName});
+    res.render('planet', { planetInfo, planetName });
 });
 
-app.get('/nasa', async(req, res) => {
+app.get('/nasa', async (req, res) => {
     let apiKey = "qdPyIOKtgEpxoLtCKdAe0mVXa9xG4UUeVpG8xGQl";
-    let todayDate = new Date().toLocaleDateString('en-CA');
-    let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${todayDate}`;
+    let date = new Date();
+    let todayDate = new Intl.DateTimeFormat('en-CA', {
+        timeZone: 'America/Los_Angeles',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    }).format(date);
     
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${todayDate}`;
+
     let response = await fetch(url);
-    let podData = await response.json(); 
+    let podData = await response.json();
 
     console.log(podData);
 
-    res.render('nasa', {podData});
+    res.render('nasa', { podData });
 });
 
 app.listen(3000, () => {
